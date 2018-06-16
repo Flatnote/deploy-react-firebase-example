90172f7f1a3d9950b89dbf1f290d6b57e3fcad7a
Chalermchai <flatsnote@gmail.com>
2018-06-16 22:51:36 +0700

    testing edit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import logo from './shapes.svg';
 import './App.css';
+import LeftColumn from './LeftColumn';
+import RightColumn from './RightColumn';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React + Firebase + Travis</h2>
+        <div className="w3-content w3-margin-top" style={{'max-width':'1400px'}}>
+          <div className="w3-row-padding">
+              <LeftColumn />
+              <RightColumn />
+          </div>
         </div>
+        <Footer />
       </div>
     );
   }
---
+++ src/Content.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+    render(){
+        return (
+            <div>
+                <p> === This is Content === </p>
+            </div>
+        );
+    }
+}
+
+export default Content;
